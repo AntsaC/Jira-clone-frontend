508b0c90503f63477a855644fc0a835c80160281
AntsaC <antsafiderana333@gmail.com>
2023-12-19 15:59:42 +0300

    Fix : Rename route to projects

--- src/config/router.jsx
+++ src/config/router.jsx
@@ -1,32 +1,32 @@
-import {createBrowserRouter} from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 import App from "../page/main/App.jsx";
 import ProjectPage from "../page/project.jsx";
 import BacklogPage from "../page/backlog.jsx";
 import SprintPage from "../page/sprint.jsx";
 
 const router = createBrowserRouter([
-    {
-        path: '/login',
-        element: <h1>Login page</h1>,
-    },
-    {
-        path: '/',
-        element: <App/>,
-        children: [
-            {
-                path: 'project',
-                element: <ProjectPage />
-            },
-            {
-                path: 'project/:key/backlog',
-                element: <BacklogPage />
-            },
-            {
-                path: 'project/:key/sprint',
-                element: <SprintPage />
-            },
-        ]
-    }
-])
+  {
+    path: "/login",
+    element: <h1>Login page</h1>,
+  },
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "projects",
+        element: <ProjectPage />,
+      },
+      {
+        path: "project/:key/backlog",
+        element: <BacklogPage />,
+      },
+      {
+        path: "project/:key/sprint",
+        element: <SprintPage />,
+      },
+    ],
+  },
+]);
 
-export default router
+export default router;
--- src/module/main/Routes.jsx
+++ src/module/main/Routes.jsx
@@ -1,32 +1,30 @@
-import {Dashboard} from "@mui/icons-material";
+import { Dashboard } from "@mui/icons-material";
 
-const createRoutes = (currentProject) => ([
-    {
-        primary: 'Dashboard',
-        path: createProjectChildrenPath(currentProject, 'dashboard'),
-        icon: <Dashboard />
-    },
-    {
-        primary: 'Project',
-        path: 'project',
-        icon: <Dashboard />
-    },
-    {
-        primary: 'Backlog',
-        path: createProjectChildrenPath(currentProject, 'backlog'),
-        icon: <Dashboard />
-    },
-    {
-        primary: 'Sprint',
-        path: createProjectChildrenPath(currentProject, 'sprint'),
-        icon: <Dashboard />
-    },
-]);
-
+const createRoutes = (currentProject) => [
+  {
+    primary: "Dashboard",
+    path: createProjectChildrenPath(currentProject, "dashboard"),
+    icon: <Dashboard />,
+  },
+  {
+    primary: "Project",
+    path: "projects",
+    icon: <Dashboard />,
+  },
+  {
+    primary: "Backlog",
+    path: createProjectChildrenPath(currentProject, "backlog"),
+    icon: <Dashboard />,
+  },
+  {
+    primary: "Sprint",
+    path: createProjectChildrenPath(currentProject, "sprint"),
+    icon: <Dashboard />,
+  },
+];
 
 function createProjectChildrenPath(projectKey, path) {
-    return `project/${projectKey}/${path}`;
+  return `project/${projectKey}/${path}`;
 }
 
 export default createRoutes;
-
