e0f83eb6578eb107a3b3cd243b10b615944e63d1
AntsaC <antsafiderana333@gmail.com>
2023-12-29 10:05:41 +0300

    Add : Story number per column in board

--- src/module/board/BoardColumn.jsx
+++ src/module/board/BoardColumn.jsx
@@ -11,9 +11,17 @@ export default function BoardColumn({ column, onDrop }) {
   return (
     <Card ref={drop} sx={{ width: "25%", minHeight: 300 }}>
       <CardContent>
-        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-          {column.name}
-        </Typography>
+        <Stack
+          direction={"row"}
+          justifyContent={"space-between"}
+          alignItems={"center"}
+        >
+          <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+            {column.name}
+          </Typography>
+          <p>{column.cards?.length}</p>
+        </Stack>
+
         <Stack mt={2} gap={2}>
           {column.cards?.map((story) => (
             <BoardItem key={story.id} story={story} currentColumn={column} />
